48ac28f2553d7b871c64efc1217c2b9082c5e2f3
Michele Rastelli <rashtao@gmail.com>
2015-07-13 18:24:55 +0200

    tests

--- test/mapping_test.js
+++ test/mapping_test.js
@@ -10,42 +10,56 @@ var types = require('../lib/MappingsTypes');
 var JsToKnex = require("../lib/JsToKnex");
 var SchemaFactory = require("../lib/SchemaFactory");
 
+var nestedSchemaChildren = {
+    text: types.text(),
+    boolean: types.boolean()
+};
+var nestedSchema = SchemaFactory.createSchema("nested_schema").keys(nestedSchemaChildren);
 
-var addressSchema = SchemaFactory.createSchema("address")
-    .keys({
-        street: types.text(),
-        city: types.text()
-    });
-
-var schema = SchemaFactory.createSchema("schema")
-    .keys({
-        uuid: types.partition_key(types.uuid(), 0),
-        boolean: types.boolean(),
-        "list_of_text": types.list(types.text()),
-        bigint: types.bigint(),
-        double: types.index(types.double()),
-        timestamp: types.timestamp(),
-        blob: types.blob(),
-        address: types.nested(addressSchema),
-        firstname: types.partition_key(types.text(), 1),
-        phones: types.map(types.text(), types.text()),
-        //addresses: types.map(types.text(), addressSchema)
-    });
+var schemaChildren = {
+    uuid: types.partition_key(types.uuid(), 0),
+    partition_key: types.partition_key(types.text(), 1),
+    boolean: types.boolean(),
+    bigint: types.bigint(),
+    double: types.double(),
+    timestamp: types.timestamp(),
+    blob: types.blob(),
+    text_index: types.index(types.text()),
+    nested_index: types.index(types.nested(nestedSchema)),
+    text_map: types.map(types.text(), types.text()),
+    text_list: types.list(types.text()),
+    nested: types.nested(nestedSchema),
+    nested_map: types.map(types.text(), types.nested(nestedSchema)),
+    nested_list: types.list(types.nested(nestedSchema))
+};
+var schema = SchemaFactory.createSchema("schema").keys(schemaChildren);
 
 
 
-var cqlFieldsForSchema = JsToKnex(schema);
-console.log(cqlFieldsForSchema);
 
-var childrenAddressSchema = JsToKnex(addressSchema);
-console.log(childrenAddressSchema);
+
+
 
 describe('Mapping Test', function () {
-    describe('conversion test', function () {
-        it("should map", function () {
-            expect(schema.keys).to.have.all.keys(cqlFieldsForSchema);
+
+    var schemaKnex = JsToKnex(schema.describe());
+    var nestedSchemaKnex = JsToKnex(nestedSchema.describe());
+
+    describe('Mapping Test', function () {
+        it("check schema names", function () {
+            expect(schema.keys).to.have.all.keys();
             expect(nestedSchema.keys).to.have.all.keys(cqlFieldsForAddressSchema);
         });
+        it("check schema children names", function () {
+            expect(schema.keys).to.have.all.keys();
+            expect(nestedSchema.keys).to.have.all.keys(cqlFieldsForAddressSchema);
+        });
+        it("check schema children types", function () {
+            expect(schema.keys).to.have.all.keys();
+            expect(nestedSchema.keys).to.have.all.keys(cqlFieldsForAddressSchema);
+        });
+
+
 
     });
 });
